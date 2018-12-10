4bbec301239da1aac7a6d700b2e94e60a549821c
Erick Nisbeth <eycsystems@live.com>
2018-12-09 20:05:26 -0600

    Creacion de Proyecto

---
+++ src/Modelos/Medicamento.java
@@ -0,0 +1,64 @@
+
+package Modelos;
+
+/**
+ *
+ * @author Erick Nisbeth
+ */
+public class Medicamento {
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private String concentracion;
+    private String presentacion;
+    private String laboratorio;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getConcentracion() {
+        return concentracion;
+    }
+
+    public void setConcentracion(String concentracion) {
+        this.concentracion = concentracion;
+    }
+
+    public String getPresentacion() {
+        return presentacion;
+    }
+
+    public void setPresentacion(String presentacion) {
+        this.presentacion = presentacion;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+    
+}
