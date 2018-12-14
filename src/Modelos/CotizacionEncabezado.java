266744a6bf655dd1ff2dde58ddc93796ec92e598
Alex Cruz <alexalfredo.cruzmiranda@gmail.com>
2018-12-13 20:59:27 -0600

    Subiendo Módulo de Cotizaciones

---
+++ src/Modelos/CotizacionEncabezado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author acruz
+ */
+public class CotizacionEncabezado {
+    
+    public String Cliente;
+    
+    public float Subtotal;
+    
+    public float Impuesto;
+    
+    public float GranTotal;
+    
+}
